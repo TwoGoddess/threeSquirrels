7a4cbd169b383f08625b191647d788693b3f3dad
aikss <2492910039@qq.com>
2017-07-10 19:22:57 +0800

    7-10

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,36 @@
 <template>
     <div id="app">
-        <sfooter></sfooter>
+        <squirrel-main></squirrel-main>
     </div>
 </template>
 
 <script>
 
-import sfooter from './pages/Footer'
+import squirrelMain from './pages/Main'
+import axios from 'axios'
 
 export default {
     name: 'app',
     components: {
-        sfooter
+        squirrelMain
     },
-    // methods: {
-    //     getData(){
-    //         axios.get("http://m.3songshu.com/mobile/api/index/info?param=%7B%22type%22%3A%22mobile%22%7D")
-    //         .then(function (res) {
-    //             console.log(res);
-    //         })
-    //         .catch(function (error) {
-    //             console.log(error);
-    //         });
-    //     }
-    // },
-    // created(){
-    //     this.getData();
-    // }
+    methods: {
+        getData(){
+            axios.get("http://m.3songshu.com/mobile/api/index/info?param=%7B%22type%22%3A%22mobile%22%7D")
+            .then(function (res) {
+                console.log(res);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }
+    },
+    created(){
+        this.getData();
+    }
 }
 </script>
 
-<style lang="less">
+<style>
     @import './assets/css/reset.css';
-    @color: red;
-    h1{
-        color: @color;
-    }
-    .footer{
-        display: flex;
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        justify-content: space-around;
-        height: 60px;
-        line-height: 60px;
-        font-size: 16px;
-        background-color: #ddd;
-    }
 </style>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -17,6 +17,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less">
+    .footer{
+        display: flex;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        justify-content: space-around;
+        height: 60px;
+        background-color: #ddd;
+        a{
+            padding-top: 40px;
+            width: 20%;
+            font-size: 10px;
+            line-height: 16px;
+            text-align: center;
+        }
+    }
 </style>
